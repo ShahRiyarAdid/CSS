970f7ab5fefa4b64df52bb7ed67ff16c772390f8
ShahRiyarAdid <riyaradidshah28@gmail.com>
2025-06-06 17:58:27 +0200

    Update accesso.js

--- accesso.js
+++ accesso.js
@@ -2,5 +2,5 @@
 document.getElementById('invia').addEventListener('click', function(event) {
     event.preventDefault(); 
     console.log('ciao');
-    window.location.href = "file:///F:/SISTEMI%20E%20RETI/CSS/mio_sito/CSS%20Comandi/Shah_riyar_adid_CSS%20Comandi.html"; 
+    window.location.href = "index.html"; 
   });
